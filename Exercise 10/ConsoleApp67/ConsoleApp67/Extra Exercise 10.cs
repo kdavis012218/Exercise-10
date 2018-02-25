78c664fb01af1c71a6cf9474db5852144ad1f109
Kevin Davis <35358468+kdavis012218@users.noreply.github.com>
2018-02-24 22:43:19 -0500

    Add files via upload

---
+++ Exercise 10/ConsoleApp67/ConsoleApp67/Extra Exercise 10.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp67
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+
+        {
+            float response;
+
+
+            Console.WriteLine("Welcome to the IncrediCoaster");
+            Console.ReadKey();
+            Console.WriteLine("You must meet the minimum height requirement to come aboard");
+            Console.ReadKey();
+            Console.WriteLine("Please enter your height in inches");
+            Console.ReadLine();
+            response = Console.ReadLine();
+            if (response => "54.0")
+            {
+                Console.WriteLine("You may pass");
+            }
+            else
+                Console.WriteLine("Come back next year when you have grown more....");
+        }
+    }
+}
